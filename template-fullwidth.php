6d212f625025d8d2deeda4238fff17244426e0cd
rcadaoas <romeo.mail7@gmail.com>
2015-02-16 21:53:50 +0400

    full page template

--- template-fullwidth.php
+++ template-fullwidth.php
@@ -7,7 +7,15 @@ Template Name: Full Width Page
 <?php get_header(); ?>
 
 <div class="container">
-	<?php the_content();?>
+	<div class="header">
+	<h1><?php the_title(); ?> </h1>
+	</div>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+	the_content();
+	endwhile; else: ?>
+	<p>Sorry, no posts matched your criteria.</p>
+	<?php endif; ?>
+	
 </div> <!-- end of #container -->
 
 <?php get_footer();?>
