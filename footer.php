c3e6ffc2c8148da5eb639e84f0ac31bda82f1463
Romeo Cadaoas <romeo.mail7@gmail.com>
2015-02-17 08:54:05 +0400

    footer
    modified the footer copyright

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
-	<div class="row">
-      <p><?php bloginfo( 'name' ); ?> is proudly powered by Wordpress and Really Minimal.</p>
+	
+
+	<div class="text-center">
+      <p>Copyright ©&nbsp;2015 · <?php bloginfo( 'name' ); ?> · powered by <a href="http://wordpress.org/" title="WordPress">WordPress</a></p>
       <?php wp_footer(); ?>
    </div>
-<div>
+
+<div> <!-- End of main div -->
+
 </body>
 </html>
