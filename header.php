69be1d137541c7f91b138e6dbf98bcc20d322eae
Romeo Cadaoas <romeo.mail7@gmail.com>
2015-02-17 08:53:28 +0400

    front page
    put the progress bars in a row. clean style css (removed the old page
    style).

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php wp_head(); ?>
 </head>
-<body >
-<div class="container">
+<body>
+<div id="main" class="container">
 
 <header>
 	<nav class="navbar navbar-inverse">
