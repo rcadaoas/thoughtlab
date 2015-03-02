d48c95d4c57247f6ef9c3c53b638f0ede62c8524
Romeo Cadaoas <romeo.mail7@gmail.com>
2015-03-02 10:04:17 +0400

    change row column size

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	</div>
 	<div class="row" >
 		<?php get_sidebar(); ?>
-		<div class="col-lg-7">
+		<div class="col-lg-9">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 				<h4>Posted on <?php the_time('F jS, Y') ?></h4>
