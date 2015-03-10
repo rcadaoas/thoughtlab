2700671ba03e5a12cb079803cb52f310eaceb061
Romeo Cadaoas <romeo.mail7@gmail.com>
2015-03-10 08:44:22 +0400

    remove wp_query

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
 	<div class="row" >
 		<?php get_sidebar(); ?>
 		<div class="col-lg-9">
-			<?php $the_query = new WP_Query( 'showposts=5' ); ?>
-			<?php if (have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 				<h4>Posted on <?php the_time('F jS, Y') ?></h4>
 				<p><?php the_content('Read More..'); ?></p>
