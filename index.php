2faec929313ff07c4d05e3ba8f2e4a0d4d3ee83a
rcadaoas <romeo.mail7@gmail.com>
2015-03-27 20:10:57 +0400

    Add widget for share icons , author and tags on the header

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 	<div class="col-lg-9">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-			<h4>Posted on <?php the_time('F jS, Y') ?></h4>
+			<p>&nbsp;&nbsp;<span class="glyphicon glyphicon-time" aria-hidden="true"></span> <?php the_time('F jS, Y') ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?php the_author(); ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<span class="glyphicon glyphicon-tag" aria-hidden="true"></span> <?php the_tags(); ?></p>
 			<?php if ( has_post_thumbnail() ) {
 				the_post_thumbnail();
 			} ?>
--- single.php
+++ single.php
@@ -2,8 +2,17 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 <h5><?php the_category('&nbsp;&rsaquo;&nbsp;'); echo "&nbsp;&rsaquo;&nbsp;"; the_title(); ?></h5>
-<div class="page-header">
-	<h1><?php the_title(); ?></h1>
+<div class="col-lg-9">
+	<div class="page-header">
+		<h1><?php the_title(); ?></h1>
+		<p>&nbsp;&nbsp;<span class="glyphicon glyphicon-time" aria-hidden="true"></span> <?php the_time('F jS, Y') ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?php the_author(); ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<span class="glyphicon glyphicon-tag" aria-hidden="true"></span> <?php the_tags(); ?></p>
+
+		<?php if ( is_active_sidebar( 'header_1' ) ) : ?>
+		<?php dynamic_sidebar( 'header_1' ); ?>
+		<?php endif; ?>	
+	</div>
 </div>
 <div class="row" >
 	<div class="col-lg-9">
