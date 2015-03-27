577eaa705ce2abd2a66d25f253686f8d26b42435
rcadaoas <romeo.mail7@gmail.com>
2015-03-27 16:34:44 +0400

    for about me page adjusments

--- template-fullwidth.php
+++ template-fullwidth.php
@@ -6,13 +6,12 @@ Template Name: Full Width Page
 
 <?php get_header(); ?>
 
-<div class="container">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 	the_content();
 	endwhile; else: ?>
 	<p>Sorry, no posts matched your criteria.</p>
 	<?php endif; ?>
 	
-</div> <!-- end of #container -->
+
 
 <?php get_footer();?>
